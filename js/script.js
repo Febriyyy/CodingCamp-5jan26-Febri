d027cbeb99de2f4e1faa6656e164d18e07ed169c
Febriyyy <febritk98@gmail.com>
2026-01-09 15:42:09 +0700

    finale

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
-let todo = [];
+let todos = [];
 
-function addTodo(){}
+function addTodo() {
+    const todoInput = document.getElementById('todo-input');
+    const todoDate = document.getElementById('todo-date');
 
-function removeTodo() {}
+    if (todoInput.value === '' || todoDate.value ==='') {
+        alert('Please fill in both the todo item and date.');
+    }else{
+        const newTodo= {
+            task:todoInput.value,
+            date:todoDate.value
+        };
 
-function filterTodoBasedDate() { }
-console.log('');
+        todos.push(newTodo);
+
+        renderTodos();
+
+        todoInput.value='';
+        todoDate.value='';
+    }
+}
+
+function renderTodos () {
+    const todoList = document.getElementById('todo-list');
+
+    todoList.innerHTML = '';
+
+    todos.forEach((todo, _) => {
+        todoList.innerHTML += `
+        <li>
+            <p class="text-2xl">${todo.task} <span class="text-sm text-gray-500">(${todo.date})</span></p>
+            <hr/>
+        </li>`;
+    });
+}
+
+function sortByDateNew() {
+  todos.sort((a, b) => new Date(a.date) - new Date(b.date));
+  renderTodos();
+}
+
+function sortByDateOld() {
+  todos.sort((a, b) => new Date(b.date) - new Date(a.date));
+  renderTodos();
+}
